36cd86f04125b57235c1888ce02f1e9323dfb0f7
Vernandio Rivaldo <vernandio.rivaldo@gmail.com>
2025-07-15 20:04:19 +0700

    Challenge Page (UI)

---
+++ app/src/main/java/edu/bluejack24_2/domojo/viewmodels/ChallengeViewModel.kt
@@ -0,0 +1,7 @@
+package edu.bluejack24_2.domojo.viewmodels
+
+import androidx.lifecycle.ViewModel
+
+class ChallengeViewModel: ViewModel() {
+
+}
---
+++ app/src/main/java/edu/bluejack24_2/domojo/views/ui/Challenge.kt
@@ -0,0 +1,15 @@
+package edu.bluejack24_2.domojo.views.ui
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import edu.bluejack24_2.domojo.R
+
+class Challenge : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_challenge)
+    }
+}
