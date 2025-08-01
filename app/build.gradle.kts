1e48fb6cea5672a5426f9a5e1642fa995949f0bc
Vernandio Rivaldo <vernandio.rivaldo@gmail.com>
2025-08-01 14:51:12 +0700

    Fix: Some things

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
 
     defaultConfig {
         applicationId = "edu.bluejack24_2.domojo"
-        minSdk = 30
+        minSdk = 35
         targetSdk = 36
         versionCode = 1
         versionName = "1.0"
